276d4d2546624522a9e2b75f83614211b45e6250
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-12-22 01:28:24 +0530

    Added GSTIN field in my account

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import Button from "@mui/material/Button";
-import { FaRegUser } from "react-icons/fa";
-import { IoIosLogOut } from "react-icons/io";
-import { RiHandbagLine } from "react-icons/ri";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { NavLink } from "react-router-dom";
 import { Divider } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { LiaGiftSolid } from "react-icons/lia";
-import { TfiWallet } from "react-icons/tfi";
-
-
+import MyAccountSideBar from "../../components/MyAccountSideBar";
 
 const MyAccount = () => {
   const [gender, setGender] = React.useState("");
@@ -24,78 +16,10 @@ const MyAccount = () => {
     setGender(event.target.value);
   };
   return (
-    <section className="w-full py-10 bg-[#f9f9f9] min-h-[80vh]">
+    <section className="w-full py-5 bg-[#f9f9f9] min-h-[80vh]">
       <div className="container flex gap-5">
         <div className="col1 w-[20%]">
-          <div className="card bg-white shadow-md rounded-md">
-            <div className="w-full p-5 flex items-center justify-center flex-col">
-              <div className="w-24 h-24 rounded-full overflow-hidden mb-3 relative group">
-                <img
-                  src="https://static.vecteezy.com/system/resources/thumbnails/027/554/917/small/shinchan-sleepy-face-illustration-free-vector.jpg"
-                  alt="Profile Image"
-                  className="w-full h-full object-cover cursor-pointer group-hover:scale-105"
-                />
-              </div>
-
-              <h4 className="text-[18px] font-semibold mb-1">Ankit Kumar</h4>
-              <h6 className="text-[12px] font-medium">username@example.com</h6>
-            </div>
-            <Divider className="mb-0!" />
-            <ul className="list-none pb-5 myAccountTabs">
-              <li className="w-full">
-                <NavLink to="/my-account" exact={true} activeClassName="active">
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <FaRegUser className="text-[18px]" />
-                    My Profile
-                  </Button>
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink to="/my-list" exact={true} activeClassName="active">
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <IoMdHeartEmpty className="text-[18px]" />
-                    My List
-                  </Button>
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink to="/my-orders" exact={true} activeClassName="active">
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <RiHandbagLine className="text-[18px]" />
-                    My Orders
-                  </Button>
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink to="/my-rewards" exact={true} activeClassName="active">
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <LiaGiftSolid className="text-[18px]" />
-                    Earned Rewards
-                  </Button>
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink
-                  to="/my-payments"
-                  exact={true}
-                  activeClassName="active"
-                >
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <TfiWallet className="text-[18px]" />
-                    Payments
-                  </Button>
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink to="/logout" exact={true} activeClassName="active">
-                  <Button className="w-full text-left! py-2! px-5! justify-start! capitalize! text-[rgba(0,0,0,0.8)]! rounded-none! flex items-center gap-2">
-                    <IoIosLogOut className="text-[18px]" />
-                    Logout
-                  </Button>
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+          <MyAccountSideBar />
         </div>
 
         <div className="col2 w-[80%]">
@@ -108,10 +32,33 @@ const MyAccount = () => {
                 <div className="flex items-center gap-5">
                   <div className="w-[50%]">
                     <TextField
-                      label="Full Name"
-                      placeholder="eg. Ankit Kumar"
+                      label="First Name"
+                      placeholder="eg. Ankit"
                       variant="outlined"
-                      defaultValue={"Ankit Kumar"}
+                      defaultValue={"Ankit"}
+                      fullWidth
+                      size="small"
+                    />
+                  </div>
+                  <div className="w-[50%]">
+                    <TextField
+                      label="Last Name (Optional)"
+                      placeholder="eg. Kumar"
+                      variant="outlined"
+                      defaultValue={"Kumar"}
+                      fullWidth
+                      size="small"
+                    />
+                  </div>
+                </div>
+                <br />
+                <div className="flex items-center gap-5">
+                  <div className="w-[50%]">
+                    <TextField
+                      label="Phone Number"
+                      placeholder="eg. 8523697425"
+                      variant="outlined"
+                      defaultValue={"9876543210"}
                       fullWidth
                       size="small"
                     />
@@ -132,10 +79,10 @@ const MyAccount = () => {
                 <div className="flex items-center gap-5">
                   <div className="w-[50%]">
                     <TextField
-                      label="Phone Number"
-                      placeholder="eg. 9876543210"
+                      label="GSTIN Number (Optional)"
+                      placeholder="Enter GST number for invoices"
                       variant="outlined"
-                      defaultValue={"9876543210"}
+                      defaultValue={"98AXWPG8963D"}
                       fullWidth
                       size="small"
                     />
