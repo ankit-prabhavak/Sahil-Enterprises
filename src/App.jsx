40cfeb89887eb2d39a0158f3b776f8d87ff74307
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-12-23 02:22:35 +0530

    Added Route for orders and fixed custom css for horizontal scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import ForgotPassword from "./Pages/ForgotPassword";
 import Checkout from "./Pages/Checkout";
 import MyAccount from "./Pages/MyAccount";
 import MyList from "./Pages/MyList";
+import Orders from "./Pages/Orders";
 
 const MyContext = createContext();
 
@@ -90,6 +91,7 @@ export default function App() {
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/my-account" element={<MyAccount />} />
             <Route path="/my-list" element={<MyList />} />
+            <Route path="/my-orders" element={<Orders />} />
           </Routes>
           <Footer />
 
