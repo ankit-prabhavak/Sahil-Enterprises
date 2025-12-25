124b5460c9fbdab56712a3bd10731e71d1890a18
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-12-26 01:39:28 +0530

    added login logic in the header

--- Admin/src/Components/Header/index.jsx
+++ Admin/src/Components/Header/index.jsx
@@ -7,13 +7,13 @@ import IconButton from "@mui/material/IconButton";
 import { FaRegBell } from "react-icons/fa";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
 import { FaRegUser } from "react-icons/fa";
 import { IoLogOutOutline } from "react-icons/io5";
 import { LuActivity } from "react-icons/lu";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MyContext } from "../../App";
-
+import { Link } from "react-router-dom";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -37,9 +37,16 @@ const Header = () => {
   const context = useContext(MyContext);
 
   return (
-    <header className={`w-full h-auto py-2 ${context.isSidebarOpen === true ? 'pl-71' : 'pl-8'} transition-all pr-8 shadow-md bg-white flex items-center justify-between`}>
+    <header
+      className={`w-full h-auto py-2 ${
+        context.isSidebarOpen === true ? "pl-71" : "pl-8"
+      } transition-all pr-8 shadow-md bg-white flex items-center justify-between`}
+    >
       <div className="part1">
-        <Button className="w-10! h-10! min-w-10! cursor-pointer!  rounded-full! text-[rgba(0,0,0,0.8)]!" onClick={() => context.setIsSidebarOpen(!context.isSidebarOpen)}>
+        <Button
+          className="w-10! h-10! min-w-10! cursor-pointer!  rounded-full! text-[rgba(0,0,0,0.8)]!"
+          onClick={() => context.setIsSidebarOpen(!context.isSidebarOpen)}
+        >
           <RiMenu2Line className="text-[18px] text-[rgba(0,0,0,0.8)]" />
         </Button>
       </div>
@@ -51,92 +58,117 @@ const Header = () => {
           </StyledBadge>
         </IconButton>
 
-        <div className="relative">
-          <div
-            className="rounded-full w-[35px] h-[35px] overflow-hidden cursor-pointer"
-            onClick={handleClickMyAccount}
-          >
-            <img
-              src="https://static.vecteezy.com/system/resources/thumbnails/027/554/917/small/shinchan-sleepy-face-illustration-free-vector.jpg"
-              alt="profile img"
-              className="w-full h-full object-cover"
-            />
-          </div>
+        {context.isLoggedIn === true ? (
+          <div className="relative">
+            <div
+              className="rounded-full w-[35px] h-[35px] overflow-hidden cursor-pointer"
+              onClick={handleClickMyAccount}
+            >
+              <img
+                src="https://static.vecteezy.com/system/resources/thumbnails/027/554/917/small/shinchan-sleepy-face-illustration-free-vector.jpg"
+                alt="profile img"
+                className="w-full h-full object-cover"
+              />
+            </div>
 
-          <Menu
-            anchorEl={anchorMyAccount}
-            id="account-menu"
-            open={openMyAccount}
-            onClose={handleCloseMyAccount}
-            onClick={handleCloseMyAccount}
-            slotProps={{
-              paper: {
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&::before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
+            <Menu
+              anchorEl={anchorMyAccount}
+              id="account-menu"
+              open={openMyAccount}
+              onClose={handleCloseMyAccount}
+              onClick={handleCloseMyAccount}
+              slotProps={{
+                paper: {
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&::before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
                   },
                 },
-              },
-            }}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          >
-            <MenuItem onClick={handleCloseMyAccount} className="bg-white!">
-              <div className="flex items-center gap-3">
-                <div
-                  className="rounded-full w-[35px] h-[35px] overflow-hidden cursor-pointer"
-                  
-                >
-                  <img
-                    src="https://static.vecteezy.com/system/resources/thumbnails/027/554/917/small/shinchan-sleepy-face-illustration-free-vector.jpg"
-                    alt="profile img"
-                    className="w-full h-full object-cover"
-                  />
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem onClick={handleCloseMyAccount} className="bg-white!">
+                <div className="flex items-center gap-3">
+                  <div className="rounded-full w-[35px] h-[35px] overflow-hidden cursor-pointer">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/027/554/917/small/shinchan-sleepy-face-illustration-free-vector.jpg"
+                      alt="profile img"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+
+                  <div className="info">
+                    <h3 className="text-[14px] font-medium leading-4">
+                      Ankit Kumar
+                    </h3>
+                    <p className="text-[12px] font-normal opacity-70">
+                      username@example.com
+                    </p>
+                  </div>
                 </div>
+              </MenuItem>
 
-                <div className="info">
-                    <h3 className="text-[14px] font-medium leading-4">Ankit Kumar</h3>
-                    <p className="text-[12px] font-normal opacity-70">username@example.com</p>
-                </div>
-              </div>
-            </MenuItem>
+              <Divider />
 
-            <Divider />
-
-            <MenuItem onClick={handleCloseMyAccount} className="flex items-center gap-3">
-              <FaRegUser className="text-[18px]" /><span className="text-[14px]">Profile</span>
-            </MenuItem>
-            <MenuItem onClick={handleCloseMyAccount} className="flex items-center gap-3">
-              <IoSettingsOutline className="text-[18px]" /><span className="text-[14px]">Settings</span>
-            </MenuItem>
-            <MenuItem onClick={handleCloseMyAccount} className="flex items-center gap-3">
-              <LuActivity className="text-[18px]" /><span className="text-[14px]">Activity Logs</span>
-            </MenuItem>
-            <Divider/>
-            <MenuItem onClick={handleCloseMyAccount} className="flex items-center gap-3">
-              <IoLogOutOutline className="text-[18px]" /><span className="text-[14px]">Logout</span>
-            </MenuItem>
-          </Menu>
-        </div>
+              <MenuItem
+                onClick={handleCloseMyAccount}
+                className="flex items-center gap-3"
+              >
+                <FaRegUser className="text-[18px]" />
+                <span className="text-[14px]">Profile</span>
+              </MenuItem>
+              <MenuItem
+                onClick={handleCloseMyAccount}
+                className="flex items-center gap-3"
+              >
+                <IoSettingsOutline className="text-[18px]" />
+                <span className="text-[14px]">Settings</span>
+              </MenuItem>
+              <MenuItem
+                onClick={handleCloseMyAccount}
+                className="flex items-center gap-3"
+              >
+                <LuActivity className="text-[18px]" />
+                <span className="text-[14px]">Activity Logs</span>
+              </MenuItem>
+              <Divider />
+              <MenuItem
+                onClick={handleCloseMyAccount}
+                className="flex items-center gap-3"
+              >
+                <IoLogOutOutline className="text-[18px]" />
+                <span className="text-[14px]">Logout</span>
+              </MenuItem>
+            </Menu>
+          </div>
+        ) : (
+          <Link to='/login'>
+          <Button className="btn-blue btn-sm rounded-full! capitalize!">
+            Login
+          </Button>
+          </Link>
+        )}
       </div>
     </header>
   );
