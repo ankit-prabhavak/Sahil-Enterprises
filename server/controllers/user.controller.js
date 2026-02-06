2fbe020c8a8d92f778c77d724a4a8cfbd322e4f2
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-02-06 23:34:23 +0530

    Made Routes and Controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,40 @@
+import UserModel from "../models/user.model.js";
+import bycrptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function registerUserController(request, response) {
+  try {
+    const { name, email, password } = request.body;
+    if (!name || !email || !password) {
+      return response.status(400).json({
+        message: "provide email, name, password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+      return response.json({
+        message: "User already Registered with this email",
+        error: true,
+        success: false,
+      });
+    }
+
+    const salt = await bycrptjs.genSalt(10);
+    const hashPassword = await bycrptjs.hash(password, salt);
+
+    
+
+
+
+
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
